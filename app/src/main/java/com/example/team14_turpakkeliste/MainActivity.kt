e623af7ab00e1074056c4a41dda2fe433256c01c
Ahmed Abdulahi Ahmed <ahmedabdulahiahmed@eduroam-193-157-169-73.wlan.uio.no>
2023-03-16 13:28:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/team14_turpakkeliste/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.team14_turpakkeliste
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.team14_turpakkeliste.ui.theme.Team14TurPakkeListeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Team14TurPakkeListeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Team14TurPakkeListeTheme {
+        Greeting("Android")
+    }
+}
