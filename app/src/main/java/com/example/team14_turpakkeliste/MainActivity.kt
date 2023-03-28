1f73a074cd90e3041452a5ea2e4a4850f1f06598
ahmedabdulahiahmed <ahmeaah.uio.no>
2023-03-28 19:48:35 +0200

    textfield for map

--- app/src/main/java/com/example/team14_turpakkeliste/MainActivity.kt
+++ app/src/main/java/com/example/team14_turpakkeliste/MainActivity.kt
@@ -25,9 +25,6 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    Greeting("Hallaballa")
-                 
                     val view = viewModel()
                     BootScreen()
                 }
