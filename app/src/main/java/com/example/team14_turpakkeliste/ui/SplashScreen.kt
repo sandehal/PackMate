be08f98b0d4480d455cf142abd891f1d6b346ac2
Alexander Løvold <palovold@ulrik.uio.no>
2023-03-31 14:28:34 +0200

    Update splashScreen

--- app/src/main/java/com/example/team14_turpakkeliste/ui/SplashScreen.kt
+++ app/src/main/java/com/example/team14_turpakkeliste/ui/SplashScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,11 +62,21 @@ fun SplashScreen(navController: NavController) {
                 .padding(40.dp)
                 .scale(scale.value)
         )
+        Text(
+            text = "PackMate",
+            textAlign = TextAlign.Center,
+            fontSize = 44.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 20.dp)
+        )
 
         Text(
             text = "Version - ${BuildConfig.VERSION_NAME}",
             textAlign = TextAlign.Center,
-            fontSize = 24.sp,
+            fontSize = 30.sp,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(16.dp)
