6302bc4f9ef811f394c305031a7b0164ad9dfb75
Alexander Løvold <palovold@ulrik.uio.no>
2023-03-31 16:06:46 +0200

    Update SavedScreen

--- app/src/main/java/com/example/team14_turpakkeliste/ui/SavedScreen.kt
+++ app/src/main/java/com/example/team14_turpakkeliste/ui/SavedScreen.kt
@@ -3,10 +3,16 @@ package com.example.team14_turpakkeliste
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 import com.example.team14_turpakkeliste.ui.theme.Burgunder
 import com.example.team14_turpakkeliste.ui.theme.Team14TurPakkeListeTheme
@@ -16,20 +22,41 @@ import com.example.team14_turpakkeliste.ui.theme.Team14TurPakkeListeTheme
 @Composable
 fun SavedScreen(navController: NavController) {
 
-    Column(
+    Column(modifier = Modifier.fillMaxSize()
     ) {
 
     }
     Column(modifier = Modifier
-        .fillMaxSize().background(color = Burgunder),
+        .fillMaxSize()
+        .background(color = Burgunder),
         verticalArrangement = Arrangement.Bottom
     ){
+        SavedButton(navController)
         BottomNavBar(navController)
     }
 
 }
 
-
+@Composable
+fun SavedButton(navController: NavController){
+    ExtendedFloatingActionButton(
+        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+        text = { Text("Hent siste pakkeliste") },
+        onClick = {  navController.navigate("ClothingScreen")
+        {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            // Avoid multiple copies of the same destination when
+            // reselecting the same item
+            launchSingleTop = true
+            // Restore state when reselecting a previously selected item
+            restoreState = true
+        }
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
 
 
 @Preview(showBackground = true)
