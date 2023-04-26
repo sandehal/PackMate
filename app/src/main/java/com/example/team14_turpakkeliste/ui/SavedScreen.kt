24a3a19bf24c5e5b9986871d17af3882c421cb9f
Alexander Løvold <palovold@ulrik.uio.no>
2023-04-26 13:04:30 +0200

    update SavedScreen

--- app/src/main/java/com/example/team14_turpakkeliste/ui/SavedScreen.kt
+++ app/src/main/java/com/example/team14_turpakkeliste/ui/SavedScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +27,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 import com.example.team14_turpakkeliste.EntityClass.AppDatabase
 import com.example.team14_turpakkeliste.ui.theme.Burgunder
+import com.example.team14_turpakkeliste.ui.theme.ForestGreen
 import com.example.team14_turpakkeliste.ui.theme.Team14TurPakkeListeTheme
 import com.example.team14_turpakkeliste.ui.theme.WhiteYellow
 import kotlinx.coroutines.launch
@@ -71,12 +74,29 @@ fun SavedScreen(navController: NavController, error: String?) {
                 .padding(1.dp)) {
                 for (s in saved){
                     item{
-                        Text(modifier = Modifier
-                            .wrapContentWidth(Alignment.CenterHorizontally)
-                            .padding(20.dp),
-                            text = "${s.firstName}, ${s.lastName}",
-                            fontSize = 18.sp
+                        ExtendedFloatingActionButton(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(20.dp),
+                            containerColor = ForestGreen,
+                            contentColor = Color.White,
+                            content = {Text("${s.firstName}, ${s.lastName}")},
+                            onClick = {navController.navigate("ListScreen")
+                            {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+
+                                // Avoid multiple copies of the same destination when
+                                // reselecting the same item
+                                launchSingleTop = true
+                                // Restore state when reselecting a previously selected item
+                                restoreState = true
+                            }
+                            }
                         )
+
+
                     }
                 }
 
