8668bdd01a0c5a954b05edc9e0f5db2d14d745c2
Alexander Løvold <palovold@ulrik.uio.no>
2023-05-23 11:53:59 +0200

    Update minAPILevel to 26

--- app/src/androidTest/java/com/example/team14_turpakkeliste/ExpandableCardTest.kt
+++ app/src/androidTest/java/com/example/team14_turpakkeliste/ExpandableCardTest.kt
@@ -3,7 +3,6 @@ package com.example.team14_turpakkeliste
 import androidx.compose.runtime.Composable
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.team14_turpakkeliste.ui.ExpandableCard
-import com.example.team14_turpakkeliste.ui.getImg
 import org.junit.Test
 import org.junit.runner.RunWith
 
